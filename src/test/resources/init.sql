85b91b784529d011c9c135c707d37a699bbbb282
ankit <akikr@duck.com>
2026-01-02 18:36:45 +0530

    Update: The app config to add a prod profile config
    Modify the application spring logback config
    Update the test init-sql schema

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS bookmarks
     url        varchar   not null,
     created_at timestamp
 );
+
+-- Index to fetch bookmarks order-by created_at: If multiple rows share same created_at, add id as tiebreaker
+CREATE INDEX idx_bookmarks_created_id ON bookmarks (created_at DESC, id DESC);
+
+-- Duplicate bookmark urls are not allowed
+CREATE UNIQUE INDEX uq_bookmarks_url ON bookmarks (url);
